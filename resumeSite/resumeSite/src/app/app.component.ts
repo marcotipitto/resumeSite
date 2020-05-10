eeab16e4a73db277b7bd777fd85b85237a28b8b3
marcotipitto <marco_tipitto@hotmail.com>
2020-05-10 12:46:22 -0500

    initial site commit

--- resumeSite/resumeSite/src/app/app.component.ts
+++ resumeSite/resumeSite/src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'resumeSite';
+
+  title = "Marco Tipitto";
+  contact: boolean;
+  collapsible: boolean;
+  collapsibleHeader: string = "+\tList of Skills and Technologies";
+
+
+  downloadResume() {
+    window.open('../assets/Resume_2019.pdf', '_blank')
+  }
+
+  openLinkedIn() {
+    window.open('https://www.linkedin.com/in/marco-tipitto-cerruti-4a8ab643/', '_blank')
+  }
+
+  contactEmail() {
+    window.location.href = "mailto:marco_tipitto@hotmail.com?subject=Subject&body=Type%20your%20message";
+  }
+
+  toggleAccordian() {
+    if (this.collapsible) {
+      this.collapsibleHeader = "-\tList of Skills and Technologies"
+      return {
+        'collapsibleActive': true
+      }
+    } else {
+      this.collapsibleHeader = "+\tList of Skills and Technologies"
+      return {
+        'collapsibleInactive': true
+      }
+    }
+  }
+
+  contactDialog() {
+    console.log('click')
+    if (this.contact) {
+      return {
+        'modalActive': true
+      }
+    } else {
+      return {
+        'modalInactive': true
+      }
+    }
+  }
 }
